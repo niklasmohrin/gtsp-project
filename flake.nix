552c00a0de6c93119835e4a4354fd594167f55a4
Niklas Mohrin <dev@niklasmohrin.de>
2024-07-17 17:22:15 +0200

    also build runner with nix

--- flake.nix
+++ flake.nix
@@ -9,12 +9,23 @@
       pkgs = import nixpkgs { inherit system; };
     in
     {
-      packages.${system}.default = pkgs.stdenvNoCC.mkDerivation {
-        name = "GTSP plots";
-        src = ./.;
-        buildInputs = with pkgs; [ R rPackages.ggplot2 rPackages.dplyr rPackages.scales ];
-        buildPhase = "R --vanilla -f plots.R";
-        installPhase = "cp out.pdf $out";
+      packages.${system} = rec {
+        runner = pkgs.rustPlatform.buildRustPackage {
+          pname = "gtsp-runner";
+          version = "0.0.1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          cargoBuildFlags = [ "--bin" "runner" ];
+        };
+
+        plots = pkgs.stdenvNoCC.mkDerivation {
+          name = "GTSP plots";
+          src = ./.;
+          buildInputs = with pkgs; [ R rPackages.ggplot2 rPackages.dplyr rPackages.scales ];
+          buildPhase = "R --vanilla -f plots.R";
+          installPhase = "cp out.pdf $out";
+        };
+        default = plots;
       };
     };
 }
