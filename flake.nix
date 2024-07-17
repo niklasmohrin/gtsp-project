f8ac0e1ed246371b924d7dac60e3ce2818548d74
Niklas Mohrin <dev@niklasmohrin.de>
2024-07-17 19:17:48 +0200

    Fetch solutions with nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,15 @@
 {
   description = "GTSP";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    gtsp-solutions = {
+      url = "file+http://www.cs.nott.ac.uk/~dxk/gtsplib/SolutionsText.zip";
+      flake = false;
+    };
+  };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, gtsp-solutions, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
@@ -18,13 +24,18 @@
           cargoBuildFlags = [ "--bin" "runner" ];
         };
 
-        plots = pkgs.stdenvNoCC.mkDerivation {
-          name = "GTSP plots";
-          src = ./.;
-          buildInputs = with pkgs; [ R rPackages.ggplot2 rPackages.dplyr rPackages.scales ];
-          buildPhase = "R --vanilla -f plots.R";
-          installPhase = "cp out.pdf $out";
-        };
+        plots =
+          let
+            fs = pkgs.lib.fileset;
+          in
+          pkgs.stdenvNoCC.mkDerivation {
+            name = "GTSP plots";
+            src = fs.toSource { root = ./.; fileset = fs.unions [ ./plots.R ./results.csv ]; };
+            buildInputs = with pkgs; [ R rPackages.ggplot2 rPackages.dplyr rPackages.scales unzip ];
+            postUnpack = "unzip -d source/solutions ${gtsp-solutions}";
+            buildPhase = "R --vanilla -f plots.R";
+            installPhase = "cp out.pdf $out";
+          };
         default = plots;
       };
     };
