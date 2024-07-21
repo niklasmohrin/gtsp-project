7582faa01cd6fe97f452f8a08d15ea97f0c4a763
Niklas Mohrin <dev@niklasmohrin.de>
2024-07-21 16:41:51 +0200

    Plot error instead of ratio

--- plots.R
+++ plots.R
@@ -15,18 +15,16 @@ results = results %>%
   }) %>%
   mutate(problem = sub(".txt", "", problem)) %>%
   summarise(
-    mean_weight = mean(weight / opt),
-    std_weight = sd(weight / opt),
+    mean_weight_err = mean((weight - opt)/ opt),
+    std_weight_err = sd((weight - opt) / opt),
   )
 
-ggplot(data = results, aes(x = name, y = mean_weight, fill = as.factor(problem))) +
+ggplot(data = results, aes(x = name, y = mean_weight_err, fill = as.factor(problem))) +
   geom_bar(stat = "identity", color = "black", position = position_dodge()) +
-  geom_errorbar(aes(ymin = mean_weight - std_weight, ymax = mean_weight + std_weight), width = 0.2,
+  geom_errorbar(aes(ymin = mean_weight_err - std_weight_err, ymax = mean_weight_err + std_weight_err), width = 0.2,
                 position = position_dodge(0.9)) +
-  geom_hline(yintercept = 1, color = "firebrick1", alpha = 0.9, linewidth = 0.4, linetype = "longdash") +
-  labs(x = "Algorithm", y = "Weight / Opt (Mean and Std-Dev)", fill = "Instance") +
+  labs(x = "Algorithm", y = "(Weight - Opt) / Opt (Mean and Std-Dev)", fill = "Instance") +
   theme_minimal() +
   theme(text = element_text(size = 10)) +
-  scale_x_discrete(labels = label_wrap(10)) +
-  coord_cartesian(ylim=c(1, NA))
+  scale_x_discrete(labels = label_wrap(10))
 ggsave("out.pdf", width = 30, height = 10, units = "cm")
